e961039598a7c1557769c0a6cbb94d6a2359cef2
mmkobylin <mmkobylin@gmail.com>
2021-06-26 20:31:37 +0100

    cleaning of app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './scss/style.scss';
-// import {
-//   BrowserRouter as Router,
-//   Route
-// } from "react-router-dom";
-import Card from './components/Card';
-import Cards from './components/Cards';
+
 import Contact from './components/Contact';
 import Header from './components/Header';
-import Job from './components/Job';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
-import Projectcard from './components/ProjectCard';
 import Skill from './components/Skill';
-import Scroll from './components/Scroll';
 import Section from './components/Section';
 import Subtitle from './components/Subtitle';
 import { Fragment } from 'react';
@@ -27,7 +19,7 @@ const App = () => {
         <Header/>
         <Subtitle subtitle = 'About'/>
         <Section 
-            message = "I am backend by day, front end by night. Currently working as an Tech Support Engineer where I spend time testing, finding bugs and providing information from database. At home I like nothing better than dive in react and css."
+            message = "I am back end by day, front end by night. Currently working as an Tech Support Engineer where I spend time testing, finding bugs and providing information from database. At home I like nothing better than dive in react and css."
         >
         </Section>
         <Subtitle subtitle = 'Skills' />
